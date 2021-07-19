6f869a702db8d1507007ff6e9bc401e6099b1532
Vishnu B <vishnub1626@gmail.com>
2021-07-19 21:12:32 +0530

    Styles cart

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,27 +1,39 @@
 <template>
     <div>
-        <div class="flex" v-for="product in cart.products" :key="product.id">
-            <img :src="product.image" :alt="product.name" class="w-40" />
-            <div class="ml-3">
-                <h2 class="text-2xl font-bold">
-                    {{ product.name }}
-                </h2>
-                <p class="text-lg font-bold">₹{{ product.price }}</p>
+        <div
+            class="flex py-5"
+            v-for="product in cart.products"
+            :key="product.id"
+        >
+            <img :src="product.image" :alt="product.name" class="w-24" />
+            <div class="flex flex-col justify-between ml-3">
+                <div class="">
+                    <h2 class="text-2xl font-bold">
+                        {{ product.name }}
+                    </h2>
+                    <p class="text-lg font-bold">₹{{ product.price }}</p>
+                </div>
                 <div>
                     <button
-                        class="p-2 text-white bg-red-800"
+                        class="p-1 text-xs text-red-600 bg-white border rounded-full "
                         @click.once="removeFromCart(product)"
                     >
-                        Remove from cart
+                        Remove
                     </button>
                 </div>
             </div>
         </div>
-        <div>
+        <div class="w-full py-4 text-2xl text-right">
             Total: ₹{{ cart.total }}
         </div>
 
-        <router-link to="/checkout" class="p-2 border">Checkout</router-link>
+        <div class="w-full py-4 text-right">
+            <router-link
+                to="/checkout"
+                class="p-2 text-white bg-blue-600 border rounded-md"
+                >Checkout</router-link
+            >
+        </div>
     </div>
 </template>
 
